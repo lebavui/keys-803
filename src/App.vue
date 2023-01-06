dc5e5eb9a08068e43eda3b4194d1c53b52f2a89f
lebavui <lebavui@gmail.com>
2023-01-06 13:46:11 +0700

    Update code

--- src/App.vue
+++ src/App.vue
@@ -1,91 +1,124 @@
-<script setup>
-import { RouterLink, RouterView } from "vue-router";
-import HelloWorld from "./components/HelloWorld.vue";
+<script>
+import { initializeApp } from "firebase/app";
+import {
+  getFirestore,
+  collection,
+  doc,
+  addDoc,
+  getDocs,
+  deleteDoc,
+} from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA1uh6C04jDSJzkF7lPSOe4_MnmpfrgJdY",
+  authDomain: "userlog803.firebaseapp.com",
+  databaseURL:
+    "https://userlog803-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "userlog803",
+  storageBucket: "userlog803.appspot.com",
+  messagingSenderId: "219849884017",
+  appId: "1:219849884017:web:818a28f6f94362a83136d0",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export default {
+  data() {
+    return {
+      keyId: 0,
+      fullname: "",
+      note: "",
+      userList: null,
+    };
+  },
+  async created() {
+    this.loadRecords();
+  },
+  methods: {
+    async addRecord() {
+      try {
+        const docRef = await addDoc(collection(db, "users"), {
+          key_id: this.keyId,
+          fullname: this.fullname,
+          note: this.note,
+          created_time: Date.now(),
+        });
+        console.log("Document written with ID: ", docRef.id);
+        this.loadRecords();
+        // Clear form
+        this.keyId = 0;
+        this.fullname = "";
+        this.note = "";
+      } catch (e) {
+        console.error("Error adding document: ", e);
+      }
+    },
+    async loadRecords() {
+      const usersCol = collection(db, "users");
+      const usersSnapshot = await getDocs(usersCol);
+      this.userList = usersSnapshot.docs.map((doc) => [doc.id, doc.data()]);
+      console.log(usersSnapshot.docs);
+      console.log(this.userList);
+    },
+    async deleteRecord(docId) {
+      await deleteDoc(doc(db, "users", docId));
+      this.loadRecords();
+    },
+  },
+};
 </script>
 
 <template>
-  <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="@/assets/logo.svg"
-      width="125"
-      height="125"
-    />
+  <div class="container">
+    <header>
+      <p>Nhập dữ liệu mới:</p>
+      <input
+        class="form-control"
+        type="number"
+        v-model="keyId"
+        placeholder="Key ID"
+      />
+      <br />
+      <input
+        class="form-control"
+        type="text"
+        v-model="fullname"
+        placeholder="Họ tên"
+      />
+      <br />
+      <input
+        class="form-control"
+        type="text"
+        v-model="note"
+        placeholder="Ghi chú"
+      />
+      <br />
+      <button class="btn btn-primary" @click="addRecord">Submit</button>
+    </header>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+    <br />
+    <p>Danh sách khóa đã tạo:</p>
+    <table class="table table-sm table-striped table-bordered">
+      <thead>
+        <tr>
+          <th>Key ID</th>
+          <th>Họ tên</th>
+          <th>Ngày tạo</th>
+          <th>Ghi chú</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="user in userList" :key="user[0]">
+          <td>{{ user[1].key_id }}</td>
+          <td>{{ user[1].fullname }}</td>
+          <td>{{ user[1].created_time }}</td>
+          <td>{{ user[1].note }}</td>
+          <td><button @click="deleteRecord(user[0])">Delete</button></td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
